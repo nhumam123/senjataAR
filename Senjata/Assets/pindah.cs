cba388ad5c0508a16ade28950ecbe8eb18ed272e
nhumam123 <naufal.pujianputra@gmail.com>
2022-06-15 06:11:55 +0700

    Update 4 obyek
    Update ini untuk menambahkan 4 objek senjata ke dalam ar yaitu keris, rencong, kujang, golok

---
+++ Senjata/Assets/pindah.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pindah : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    public void LoadToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void QuitApps()
+    {
+        Application.Quit();
+    }
+}
